33d65a4a469d256913c1baf495d61f10c8798fa1
Chaybe <chaybe_lucas@hotmail.com>
2020-04-10 13:43:58 -0400

    atualizaçao pra deixar o site bonitão

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+
+  </main>
+  
+</div>
+
+    <script src="../plugin/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../plugin/fontawesome/js/all.min.js"></script>
+  </body>
+</html>
---
+++ view/perfil.php
@@ -0,0 +1,10 @@
+<?php require_once("header.php"); ?>
+
+<div class="corpo">
+    <div class="titulo-programa">Sobre mim</div><br/>
+    <div class="descricao-programa">
+        Trabalhando para aperfeiçoar o mundo
+    </div>
+</div>
+
+<?php require_once("footer.php"); ?>
